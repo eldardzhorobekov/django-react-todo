648c10c4525becedcaae3d1bee1ee74b7921abcf
mio-eldar <eldar.djorobekov@gmail.com>
2021-03-13 14:42:02 +0600

    Heroku app deployment

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -7,3 +7,15 @@ from todo.models import Todo
 class TodoViewset(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
